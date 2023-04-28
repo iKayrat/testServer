992fb138950bf254b388f7be340fad1c3c332630
k isaev <ik.kairat@gmail.com>
2023-04-28 17:33:17 +0600

    edit and add new logic case

--- main.go
+++ main.go
@@ -6,8 +6,10 @@ import (
 	"time"
 )
 
-const ProbableMaximumUsageCPU = 120
-const ProbableMinimumUsageCPU = 80
+const (
+	ProbableMaximumUsageCPU = 120
+	ProbableMinimumUsageCPU = 80
+)
 
 type Server struct {
 	Address     string
@@ -53,104 +55,141 @@ type Room struct {
 // 	return servers, nil
 // }
 
-func newRoom(room Room, servers []Server) string {
+func newRoom(room Room) string {
 
+	// IT'S COMMENTED, BECAUSE WE USE GLOBAL VARIABLE - "servers"
 	// servers:=getFromRedis()
 
 	// temporary server to find maximum cpu usage
-	maxServer := Server{}
+	suitServer := Server{}
+
+	cpuUsage := 0
+	if room.Duration == 60 {
+		cpuUsage = 80
+	} else {
+		cpuUsage = 120
+	}
 
 	// Find available server with maximum CPU Usage
 	for _, server := range servers {
 
-		// if has rooms
-		if len(server.Rooms) != 0 {
-			// if usage fit in server capacity
-			hasMinUsage := server.CPUusage+ProbableMaximumUsageCPU <= server.CPUcapacity
-			if hasMinUsage && server.CPUusage > maxServer.CPUusage && server.Status {
-				maxServer = server
+		isAvailable := server.CPUusage+cpuUsage <= server.CPUcapacity
+		length := len(server.Rooms)
+
+		if server.Status {
+
+			switch {
+			case isAvailable && length != 0:
+				suitServer = server
+
+			case isAvailable && length == 0:
+				suitServer = server
 
 			}
-			// if has no rooms
-		} else if server.Status {
-			maxServer = server
+		} else {
+			if !server.Status && suitServer.Address == "" {
+				server.Status = true
+				suitServer = server
+			}
 		}
 	}
 
+	if suitServer.Address == "" && suitServer.CPUcapacity == 0 {
+		suitServer.Address = "newserver"
+		suitServer.CPUcapacity = 800
+		suitServer.Status = true
+
+		servers = append(servers, suitServer)
+	}
+
 	// add Room to found server
-	maxServer.Rooms = append(maxServer.Rooms, Room{Id: room.Id, Duration: room.Duration})
+	suitServer.Rooms = append(suitServer.Rooms, Room{Id: room.Id, Duration: room.Duration})
 
 	// random cpu usage
-	cpuUsage := RandomInt(ProbableMinimumUsageCPU, ProbableMaximumUsageCPU)
-	maxServer.CPUusage += cpuUsage
+	suitServer.CPUusage += cpuUsage
 
 	for i := 0; i < len(servers); i++ {
 
 		// find servers with no rooms and turn off
 		if len(servers[i].Rooms) == 0 {
+			servers[i].CPUusage = 0
 			servers[i].Status = false
 		}
 
-		// update choosed server
-		if servers[i].Address == maxServer.Address {
-			servers[i].Address = maxServer.Address
-			servers[i].CPUcapacity = maxServer.CPUcapacity
+		// update chosen server
+		if servers[i].Address == suitServer.Address {
+			servers[i].Address = suitServer.Address
+			servers[i].CPUcapacity = suitServer.CPUcapacity
 			servers[i].CPUusage += cpuUsage
-			servers[i].Status = maxServer.Status
-			servers[i].Rooms = maxServer.Rooms
+			servers[i].Status = suitServer.Status
+			servers[i].Rooms = suitServer.Rooms
 		}
 	}
 
-	return maxServer.Address
+	return suitServer.Address
+}
+
+var servers = []Server{
+	{
+		Address:     "server1",
+		CPUcapacity: 800,
+		CPUusage:    0,
+		Status:      true,
+		Rooms:       []Room{},
+	},
+	{
+		Address:     "server2",
+		CPUcapacity: 800,
+		CPUusage:    0,
+		Status:      false,
+		Rooms:       []Room{},
+	},
+	{
+		Address:     "server3",
+		CPUcapacity: 800,
+		CPUusage:    80,
+		Status:      true,
+		Rooms:       []Room{{Id: 1111, Duration: 60}},
+	},
+	{
+		Address:     "server4",
+		CPUcapacity: 800,
+		CPUusage:    120,
+		Status:      true,
+		Rooms:       []Room{{Id: 1112, Duration: 180}},
+	},
 }
 
 func main() {
 
-	// example of server with parametres
-	var servers = []Server{
-		{
-			Address:     "server1",
-			CPUcapacity: 800,
-			CPUusage:    0,
-			Status:      true,
-			Rooms:       []Room{},
-		},
-		{
-			Address:     "server2",
-			CPUcapacity: 800,
-			CPUusage:    0,
-			Status:      false,
-			Rooms:       []Room{},
-		},
-		{
-			Address:     "server3",
-			CPUcapacity: 800,
-			CPUusage:    80,
-			Status:      true,
-			Rooms:       []Room{{Id: int64(RandomInt(1000, 1200)), Duration: 60}},
-		},
-		{
-			Address:     "server4",
-			CPUcapacity: 800,
-			CPUusage:    120,
-			Status:      true,
-			Rooms: []Room{
-				{
-					Id:       int64(RandomInt(1000, 1200)),
-					Duration: 180,
-				},
-			},
-		},
-	}
-
 	// example of room for testing
 	room := Room{
-		Id:       int64(RandomInt(1000, 1200)),
+		Id:       2222,
+		Duration: 180,
+	}
+	room1 := Room{
+		Id:       2223,
 		Duration: 60,
 	}
+	room2 := Room{
+		Id:       2224,
+		Duration: 180,
+	}
 
-	serverAddress := newRoom(room, servers)
+	serverAddress := newRoom(room)
+	serverAddress1 := newRoom(room1)
+	serverAddress2 := newRoom(room2)
+	serverAddress3 := newRoom(room1)
+	serverAddress4 := newRoom(room2)
+	serverAddress5 := newRoom(room2)
+	serverAddress6 := newRoom(room)
 	fmt.Println(serverAddress)
+	fmt.Println(serverAddress1)
+	fmt.Println(serverAddress2)
+	fmt.Println(serverAddress3)
+	fmt.Println(serverAddress4)
+	fmt.Println(serverAddress5)
+	fmt.Println(serverAddress6)
 
 }
 
--- main_test.go
+++ main_test.go
@@ -1,88 +1,160 @@
 package main
 
 import (
-	"math/rand"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
-
-var testcaseServers = []Server{
-	{
-		Address:     "server1",
-		CPUcapacity: 800,
-		CPUusage:    0,
-		Status:      true,
-		Rooms:       []Room{},
-	},
-	{
-		Address:     "server2",
-		CPUcapacity: 800,
-		CPUusage:    0,
-		Status:      false,
-		Rooms:       []Room{},
-	},
-	{
-		Address:     "server3",
-		CPUcapacity: 800,
-		CPUusage:    80,
-		Status:      true,
-		Rooms:       []Room{{Id: rand.Int63n(10), Duration: 60}},
-	},
-	{
-		Address:     "server4",
-		CPUcapacity: 800,
-		CPUusage:    120,
-		Status:      true,
-		Rooms:       []Room{{Id: rand.Int63n(10), Duration: 180}},
-	},
-}
 
 var testcaseRoom = []Room{
 	{
 		Id:       1,
 		Duration: 60,
 	},
-	// {
-	// 	Id:       2,
-	// 	Duration: 180,
-	// },
-	// {
-	// 	Id:       3,
-	// 	Duration: 60,
-	// },
+	{
+		Id:       2,
+		Duration: 180,
+	},
 }
 
+func TestHasRooms(t *testing.T) {
+
+	want1 := "server4"
+	want2 := "server4"
+	want3 := "server4"
+
+	// add rooms to server4
+	actual1 := newRoom(testcaseRoom[0])
+	actual2 := newRoom(testcaseRoom[0])
+	actual3 := newRoom(testcaseRoom[0])
+
+	require.Equal(t, want1, actual1)
+	require.Equal(t, want2, actual2)
+	require.Equal(t, want3, actual3)
+
+}
+func TestServer4Full(t *testing.T) {
+
+	want1 := "server4"
+	want2 := "server3"
+
+	// filling server4 manually
+	actual1 := newRoom(testcaseRoom[0])
+	require.Equal(t, want1, actual1)
+	for i := 0; i < 5; i++ {
+		actual2 := newRoom(testcaseRoom[1])
+		require.Equal(t, want1, actual2)
+	}
+
+	// TEST WE WANT
+	// added room to server3 becasue server4 is full
+	actual2 := newRoom(testcaseRoom[1])
+	require.Equal(t, want2, actual2)
+
+}
+
+// When rooms filled in server3,4 , adds into working server
 func TestNoRooms(t *testing.T) {
 
-	want := "server1"
+	want1 := "server4"
+	want2 := "server3"
+	want3 := "server1"
 
-	tc := testcaseServers[:2]
-
-	for _, tr := range testcaseRoom {
-		actual := newRoom(tr, tc)
-
-		require.Equal(t, want, actual)
+	// filling server4 manually
+	actual1 := newRoom(testcaseRoom[0])
+	require.Equal(t, want1, actual1)
+	for i := 0; i < 5; i++ {
+		actual1 := newRoom(testcaseRoom[1])
+		require.Equal(t, want1, actual1)
 	}
+
+	// filling server3 manually
+	for i := 0; i < 6; i++ {
+		actual2 := newRoom(testcaseRoom[1])
+		require.Equal(t, want2, actual2)
+	}
+
+	// TEST WE WANT
+	actual3 := newRoom(testcaseRoom[1])
+	require.Equal(t, want3, actual3)
+
 }
 
-func TestUsageOverCapacity(t *testing.T) {
+func TestNoAnyRunningServer(t *testing.T) {
 
-	want := "server1"
+	want1 := "server4"
+	want2 := "server3"
+	want3 := "server1"
+	want4 := "server2"
 
-	tc := testcaseServers
-	tc[2].CPUusage = 800
-	tc[3].CPUusage = 800
-
-	for _, tr := range testcaseRoom {
-		actual := newRoom(tr, tc)
-
-		require.Equal(t, want, actual)
-
-		if want != actual {
-			t.Error("not equal")
-		}
-
+	// filling server4 manually
+	actual1 := newRoom(testcaseRoom[0])
+	require.Equal(t, want1, actual1)
+	for i := 0; i < 5; i++ {
+		actual1 := newRoom(testcaseRoom[1])
+		require.Equal(t, want1, actual1)
 	}
+
+	// filling server3 manually
+	for i := 0; i < 6; i++ {
+		actual2 := newRoom(testcaseRoom[1])
+		require.Equal(t, want2, actual2)
+	}
+
+	//filling server1
+	actual3 := newRoom(testcaseRoom[0])
+	require.Equal(t, want3, actual3)
+	for i := 0; i < 6; i++ {
+		actual3 := newRoom(testcaseRoom[1])
+		require.Equal(t, want3, actual3)
+	}
+
+	// TEST WE WANT
+	actual4 := newRoom(testcaseRoom[1])
+	require.Equal(t, want4, actual4)
+
+}
+
+func TestNoAnyServer(t *testing.T) {
+
+	want1 := "server4"
+	want2 := "server3"
+	want3 := "server1"
+	want4 := "server2"
+	want5 := "newserver"
+
+	// filling server4 manually
+	actual1 := newRoom(testcaseRoom[0])
+	require.Equal(t, want1, actual1)
+	for i := 0; i < 5; i++ {
+		actual1 := newRoom(testcaseRoom[1])
+		require.Equal(t, want1, actual1)
+	}
+
+	// filling server3 manually
+	for i := 0; i < 6; i++ {
+		actual2 := newRoom(testcaseRoom[1])
+		require.Equal(t, want2, actual2)
+	}
+
+	//filling server1
+	actual3 := newRoom(testcaseRoom[0])
+	require.Equal(t, want3, actual3)
+	for i := 0; i < 6; i++ {
+		actual3 := newRoom(testcaseRoom[1])
+		require.Equal(t, want3, actual3)
+	}
+
+	// filling server2
+	actual4 := newRoom(testcaseRoom[0])
+	require.Equal(t, want4, actual4)
+	for i := 0; i < 6; i++ {
+		actual4 := newRoom(testcaseRoom[1])
+		require.Equal(t, want4, actual4)
+	}
+
+	// TEST WE WANT
+	actual5 := newRoom(testcaseRoom[0])
+	require.Equal(t, want5, actual5)
 
 }
